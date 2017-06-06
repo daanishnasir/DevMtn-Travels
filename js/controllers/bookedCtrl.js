34a6e47779b094547ce4e5d420596049657eff1b
daanishnasir <daanishnasir@gmail.com>
2017-06-06 10:30:20 -0500

    cleaner

--- js/controllers/bookedCtrl.js
+++ js/controllers/bookedCtrl.js
@@ -1,3 +1,18 @@
-angular.module('devmtnTravel').controller('bookedCtrl', function($scope, mainSrv){
+angular.module('devmtnTravel').controller('bookedCtrl', function($scope, mainSrv, $stateParams){
+
+
+  var id = parseInt($stateParams.id)
+
+  var packageInfo = mainService.packageInfo
+  
+  for (var i = 0; i < packageInfo.length; i++) {
+    if (packageInfo[i].id === id) {
+      $scope.foundCity = packageInfo[i].city
+      $scope.foundCountry = packageInfo[i].country
+    }
+  }
+
+
+
 
 });
